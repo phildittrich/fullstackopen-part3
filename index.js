684638d1a062803dfc6caa2cae3abccca7424895
Philipp Dittrich <mail@philippdittri.ch>
2022-10-12 11:58:16 +0200

    ex 3.6

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
   { 
     "id": 1,
@@ -51,6 +53,31 @@ app.delete('/api/persons/:id', (request, response) => {
   persons = persons.filter(person => person.id !== id)
 
   response.status(204).end()
+})
+
+const generateId = () => Math.floor(Math.random() * 10e8)
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  const errors = []
+  
+  if(!body.name) errors.push('name missing')
+  if(!body.number) errors.push('number missing')
+  if(persons.filter( person => person.name === body.name ).length) errors.push('person exists')
+
+  if(errors.length > 0) {
+    return response.status(400).json(errors)
+  }
+
+  const person = {
+    id: generateId(),
+    name: body.name,
+    number: body.number
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
 })
 
 const PORT = 3001
