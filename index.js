0000ed3bcffd524def464c1f30e2d430f1f16f87
Philipp Dittrich <mail@philippdittri.ch>
2022-10-12 10:53:37 +0200

    ex 3.2

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const http = require('http')
+const express = require('express')
+const app = express()
 
 let persons = [
   { 
@@ -23,11 +24,18 @@ let persons = [
   }
 ];
 
-const app = http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'application/json' })
-  response.end(JSON.stringify(persons))
+app.get('/info', (request, response) => {
+  response.send(`
+    <p>Phonebook has info for ${persons.length} people</p>
+    <p>${new Date()}</p>
+  `)
+})
+
+app.get('/api/persons', (request, response) => {
+  response.json(persons)
 })
 
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () =>{
+  console.log(`Server running on port ${PORT}`)
+})
